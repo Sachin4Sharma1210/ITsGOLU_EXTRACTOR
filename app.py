585d0e1d1b8600e79165bd087ed962410c56abb5
Sachin4Sharma1210 <rajbala281210@gmail.com>
2026-03-23 20:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Tech VJ'
+    return 'Hello from SACHIN SHARMA'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
